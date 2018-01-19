5bc74a2ba5738c9c64a20f70332072dbaf78ad2d
Cagliostro <atfield2501@gmail.com>
2018-01-19 12:37:42 +0100

    //Ajout du script

---
+++ magik_html.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- coding: utf8
+# Auteur: Cagliostro <atfield2501@gmail.com>
+# Script générateur de squelette de page html.
+
+# Evolution : passer un parametre pour le nombre de quelettes à generer.
+
+
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+   <head>
+       <title>*******</title>
+       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+       <style type="text/css">
+       a img { border:0; }
+       </style>
+   </head>
+   <body><p>' >> squelette.html
+
+
