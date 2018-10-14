93be8361c5bf906229cfde9d9f3da3aee995eb6c
~jusdepatate <yaume@ntymail.com>
2018-10-14 17:12:48 +0200

    bon, wtf les tabs se sont pas mis :thinking:

--- magik_html.sh
+++ magik_html.sh
@@ -3,22 +3,22 @@
 # Auteur: Cagliostro <atfield2501@gmail.com>
 # Script générateur de squelette de page html.
 
-# Evolution : passer un parametre pour le nombre de quelettes à generer.
+# Evolution : passer un parametre pour le nombre de squelettes à generer.
 # Fait par Jus de Patate <yaume@ntymail.com> github:jusdepatate
 
 text='<!DOCTYPE html>
 <html lang="fr">
-        <head>
-                <title>*******</title>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                <style type="text/css">
-                body {
-                }
-                </style>
-        </head>
-        <body>
-                <p>
-                </p>'
+	<head>
+		<title>*******</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<style type="text/css">
+			body {
+			}
+		</style>
+	</head>
+	<body>
+		<p>
+		</p>'
 echo "$text" > squelette-0.html
 
 echo "Une page a été créée,"
@@ -27,22 +27,22 @@ ask () {                        # $1=question $2=options
  # Gros merci a https://stackoverflow.com/a/20817520
     # set REPLY
     # options: x=..|y=..
-    while $(true); do
-        printf '%s [%s] ' "$1" "$2"
-        stty cbreak
-        REPLY=$(dd if=/dev/tty bs=1 count=1 2> /dev/null)
-        stty -cbreak
-        test "$REPLY" != "$(printf '\n')" && printf '\n'
-        (
-            IFS='|'
-            for o in $2; do
-                if [ "$REPLY" = "${o%%=*}" ]; then
-                    printf '\n'
-                    break
-                fi
-            done
-        ) | grep ^ > ~/null && rm ~/null && return
-    done
+	while $(true); do
+		printf '%s [%s] ' "$1" "$2"
+		stty cbreak
+		REPLY=$(dd if=/dev/tty bs=1 count=1 2> /dev/null)
+		stty -cbreak
+		test "$REPLY" != "$(printf '\n')" && printf '\n'
+		(
+			IFS='|'
+			for o in $2; do
+				if [ "$REPLY" = "${o%%=*}" ]; then
+					printf '\n'
+				break
+				fi
+			done
+		) | grep ^ > ~/null && rm ~/null && return
+	done
 }
 
 ask 'Voulez-vous en faire d autres ?' 'o=oui|n=non'
